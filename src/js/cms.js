56b78d4fc124437fadeb0b2a63c49555bdd33e85
joeman <manshunwing@gmail.com>
2023-02-09 12:33:35 +0800

    delete the preview

--- src/js/cms.js
+++ src/js/cms.js
@@ -11,9 +11,5 @@ import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
 
 CMS.registerPreviewStyle(styles, { raw: true });
-CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
-CMS.registerPreviewTemplate("values", ValuesPreview);
-CMS.registerPreviewTemplate("contact", ContactPreview);
+
 CMS.init();
